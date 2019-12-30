e30bbf35a1c0bfb2ae7a1837ce6ba62cdaabdb66
Eugene Sadocoi <evgeni@eniks.com>
2019-12-30 12:41:35 -0800

    Policy enforcement

--- src/Policy/IPolicyList.cs
+++ src/Policy/IPolicyList.cs
@@ -13,7 +13,7 @@ namespace Unity.Policy
         /// <param name="type">Type of the registration</param>
         /// <param name="policyInterface">Type of policy to retrieve</param>
         /// <returns>Instance of the policy or null if none found</returns>
-        object? Get(Type? type, Type policyInterface);
+        object? Get(Type type, Type policyInterface);
 
         /// <summary>
         /// Get policy
@@ -30,7 +30,7 @@ namespace Unity.Policy
         /// <param name="type">Type of the registration</param>
         /// <param name="policyInterface">Type of policy to be set</param>
         /// <param name="policy">Policy instance to be set</param>
-        void Set(Type? type, Type policyInterface, object policy);
+        void Set(Type type, Type policyInterface, object policy);
 
         /// <summary>
         /// Set policy
