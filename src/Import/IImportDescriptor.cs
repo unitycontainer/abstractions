36b85e8691870325d03eaafbe67068627800017c
ENikS <evgeni@eniks.com>
2023-03-19 19:05:39 -0700

    Fixing nested injection

--- src/Import/IImportDescriptor.cs
+++ src/Import/IImportDescriptor.cs
@@ -33,59 +33,35 @@ namespace Unity.Extension
         #region Metadata
 
         /// <summary>
-        /// True if annotated with <see cref="DependencyResolutionAttribute"/>
+        /// True if annotated with <see cref="Unity.DependencyResolutionAttribute"/>
         /// </summary>
         bool IsImport { get; set; }
-
-        #endregion
-
-
-        #region Arguments
-
-        IList Arguments { set; }
         
-        #endregion
-
-
-        #region Default Value
 
         /// <summary>
         /// Allows default value if can not be resolved
         /// </summary>
         bool AllowDefault { get; set; }
 
+
         /// <summary>
         /// Sets default value and flips <see cref="AllowDefault"/> to <see cref="True"/>
         /// </summary>
         object? Default { set; }
 
-        #endregion
 
+        object?[]? Arguments { set; }
 
-        #region Value Setters
 
         /// <summary>
-        /// Set import value
-        /// </summary>
-        /// <remarks>
-        /// A value set by this setter will be injected as is, with no additional processing.
-        /// </remarks>
-        object? Value { set; }
-
-        /// <summary>
-        /// Set dynamic (unknown) value
+        /// Sets Data value for current import
         /// </summary>
         /// <remarks>
         /// This setter used when runtime value is not known at design time. The type of the value
         /// will be analyzed at runtime. The value could be a resolver, another <see cref="InjectionMember"/>, 
         /// and etc.
         /// </remarks>
-        object? Dynamic { set; }
-
-        /// <summary>
-        /// Indicates that no data is injected
-        /// </summary>
-        void None();
+        object? Data { set; }
 
         #endregion
     }
