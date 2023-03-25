b06d4e3c2da53bd124be635c48a3c3f02c60d26e
ENikS <evgeni@eniks.com>
2023-03-25 11:22:39 -0700

    Compiling pipelines and factories

--- src/Resolution/IResolverFactory.cs
+++ src/Resolution/IResolverFactory.cs
@@ -1,8 +1,14 @@
-﻿namespace Unity.Resolution;
+﻿using System;
 
+namespace Unity.Resolution;
+
+
+public delegate ResolveDelegate<TContext> ResolverFactory<TContext>(Type type)
+    where TContext : IResolveContext;
 
 
 public interface IResolverFactory { }
+
 
 public interface IResolverFactory<in TMemberInfo> : IResolverFactory
 {
