4fc9a80ecc3704b12bfb6bf39d906b2ab45061f6
ENikS <evgeni@eniks.com>
2023-04-02 08:44:26 -0700

    Delegates refactoring

--- src/Resolution/IResolverFactory.cs
+++ src/Resolution/IResolverFactory.cs
@@ -1,10 +1,5 @@
-﻿using System;
+﻿namespace Unity.Resolution;
 
-namespace Unity.Resolution;
-
-
-public delegate ResolveDelegate<TContext> ResolverFactory<TContext>(Type type)
-    where TContext : IResolveContext;
 
 
 public interface IResolverFactory { }
---
+++ src/Resolution/ResolverFactory.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace Unity.Resolution;
+
+
+public delegate ResolveDelegate<TContext> ResolverFactory<TContext>(Type type)
+    where TContext : IResolveContext;
